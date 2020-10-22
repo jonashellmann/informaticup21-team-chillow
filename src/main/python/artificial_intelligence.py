002d0dd46f22e6033302618fe1fbdc2b01370425
Jonas Hellmann <hellmann.jonas@web.de>
2020-10-22 21:14:09 +0200

    Ergaenze Typ-Sicherheit in Rueckgabewerten und Parametern

--- src/main/python/artificial_intelligence.py
+++ src/main/python/artificial_intelligence.py
@@ -3,17 +3,18 @@ import random
 from abc import ABCMeta, abstractmethod
 
 from src.main.python.model.action import Action
+from src.main.python.model.game import Game
 
 
 class ArtificialIntelligence(metaclass=ABCMeta):
 
     @abstractmethod
-    def create_next_action(self, data):
+    def create_next_action(self, game: Game) -> Action:
         raise NotImplementedError
 
 
 class ChillowAI(ArtificialIntelligence):
 
-    def create_next_action(self, data):
+    def create_next_action(self, game: Game) -> Action:
         # Todo: Implement
         return random.choice(list(Action))
