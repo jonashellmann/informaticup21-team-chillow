663a3c1a42ca3201acc4cef660eeeea446f4259f
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-25 20:06:25 +0100

    Farbe vom ersten Spieler der Liste anzeigen im Pygame, damit zwischen  möglichen Farben entschieden wird

--- chillow/model/cell.py
+++ chillow/model/cell.py
@@ -9,4 +9,4 @@ class Cell:
     players: Player = None  # List of players
 
     def get_player_id(self) -> int:
-        return 0 if self.players is None else self.players.id
+        return 0 if self.players is None else self.players[0].id
