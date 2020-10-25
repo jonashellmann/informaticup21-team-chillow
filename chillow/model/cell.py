31b5efd4e470fed9eb05e3ba03faa8456f19cbfd
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-25 20:15:42 +0100

    cell List als Datentypen für Player geben

--- chillow/model/cell.py
+++ chillow/model/cell.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import List
 
 from chillow.model.player import Player
 
@@ -6,7 +7,7 @@ from chillow.model.player import Player
 @dataclass
 class Cell:
 
-    players: Player = None  # List of players
+    players: List[Player] = None  # List of players
 
     def get_player_id(self) -> int:
         return 0 if self.players is None else self.players[0].id
