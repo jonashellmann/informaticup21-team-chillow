f56d25ba4311f698c004cbf2f4a0ddd1b66b2750
Jonas Hellmann <hellmann.jonas@web.de>
2020-10-28 12:46:51 +0100

    Erstelle Knoten-Klasse zur Erzeugung einer Baumstruktur

--- chillow/ai/alpha_beta_pruning_ai.py
+++ chillow/ai/alpha_beta_pruning_ai.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+from typing import List
+
 from chillow.ai.artificial_intelligence import ArtificialIntelligence
 from chillow.model.action import Action
 from chillow.model.game import Game
@@ -11,4 +14,15 @@ class AlphaBetaPruningAI(ArtificialIntelligence):
         self.depth = depth
 
     def create_next_action(self, game: Game) -> Action:
+        root = AlphaBetaNode(game)
         raise NotImplementedError
+
+
+@dataclass
+class AlphaBetaNode(object):
+
+    data: Game
+    children = []
+
+    def append_child(self, node):
+        self.children.append(node)
