175dd02888b976650d79b10e7b3ac37be5c2f87f
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-29 18:41:40 +0100

    Chillow_AI wieder entfernen, die beim Mergen hinzugefügt wurde

--- chillow/ai/artificial_intelligence.py
+++ chillow/ai/artificial_intelligence.py
@@ -15,10 +15,3 @@ class ArtificialIntelligence(metaclass=ABCMeta):
     @abstractmethod
     def create_next_action(self, game: Game) -> Action:
         raise NotImplementedError
-
-
-class ChillowAI(ArtificialIntelligence):
-
-    def create_next_action(self, game: Game) -> Action:
-        # Todo: Implement
-        return random.choice(list(Action))
