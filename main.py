ddc366fcd5c72243aeaa6bc63ca5d5132b2e85e1
Jonas Hellmann <hellmann.jonas@web.de>
2020-10-30 15:16:46 +0100

    Nutze Logging-Framework und setze Log-Level auf Warning

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+import logging
 
 from chillow.connection import OnlineConnection, OfflineConnection
 
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.WARNING)
 
 if not os.getenv('PLAY_ONLINE', False):
     con = OfflineConnection()
