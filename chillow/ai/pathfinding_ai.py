ab214248c0958fa6addbd7c8d4de0e1e05f2b91a
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-30 18:58:49 +0100

    Codequalität

--- chillow/ai/pathfinding_ai.py
+++ chillow/ai/pathfinding_ai.py
@@ -1,5 +1,9 @@
 from random import choice
 
+from pathfinding.core.diagonal_movement import DiagonalMovement
+from pathfinding.core.grid import Grid
+from pathfinding.finder.a_star import AStarFinder
+
 from chillow.ai.artificial_intelligence import ArtificialIntelligence
 from chillow.model.action import Action
 from chillow.model.game import Game
@@ -7,7 +11,7 @@ from chillow.model.player import Player
 from chillow.service.game_service import GameService
 
 
-class Pahtfinding_AI(ArtificialIntelligence):
+class PahtfindingAI(ArtificialIntelligence):
 
     def __init__(self, player: Player, game: Game, max_speed):
         super().__init__(player)
@@ -22,4 +26,7 @@ class Pahtfinding_AI(ArtificialIntelligence):
 
         surviving_actions = self.find_surviving_actions(game_service)
 
-        return choice(surviving_actions) if len(surviving_actions) > 0 else choice()
+        return choice(surviving_actions) if len(surviving_actions) > 0 else Action.get_random_action()
+
+    def find_action_by_best_path_connection(self):
+        pass
