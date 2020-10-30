1b503725c79e16800c0d9bd720fd86b00f390eb5
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-30 15:16:52 +0100

    Grundstruktur pathfinding ai

---
+++ chillow/ai/pathfinding_ai.py
@@ -0,0 +1,22 @@
+from chillow.ai.artificial_intelligence import ArtificialIntelligence
+from chillow.model.action import Action
+from chillow.model.game import Game
+from chillow.model.player import Player
+from chillow.service.game_service import GameService
+
+
+class Pahtfinding_AI(ArtificialIntelligence):
+
+    def __init__(self, player: Player, game: Game, max_speed):
+        super().__init__(player)
+        self.__game = game
+        self.turn_ctr = 0
+        self.__max_speed = max_speed
+
+    def create_next_action(self, game: Game) -> Action:
+        self.turn_ctr += 1
+        game_service = GameService(game)
+        game_service.turn.turn_ctr = self.turn_ctr
+
+        surviving_actions = self.find_surviving_actions(game_service)
+
