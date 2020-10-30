a99750cb440e1a16c552e75396837e0c371bcdaf
Jonas Hellmann <hellmann.jonas@web.de>
2020-10-30 10:50:02 +0100

    Ergaenze Test fuer neue Mehtode in Game-Klasse

--- tests/model/test_game.py
+++ tests/model/test_game.py
@@ -110,11 +110,36 @@ class GameTest(unittest.TestCase):
 
         self.assertEqual(player1, game.get_player_by_id(1))
 
-    def test_player_with_id_should_be_returned(self):
-        player1 = Player(1, 0, 0, Direction.up, 0, True, "Name")
-        player2 = Player(2, 1, 0, Direction.up, 0, True, "Name")
-        cells = [[Cell([player1]), Cell([player2])]]
+    def test_raise_exception_when_player_id_invalid(self):
+        player1 = Player(1, 1, 0, Direction.up, 0, True, "Name")
+        player2 = Player(2, 0, 0, Direction.up, 0, True, "Name")
+        cells = [[Cell([player2]), Cell([player1])]]
         game = Game(2, 1, cells, [player1, player2], 1, True, datetime.now())
 
         with self.assertRaises(PlayerWithGivenIdNotAvailableException):
             game.get_player_by_id(100)
+
+    def test_return_all_players_except_one(self):
+        player1 = Player(1, 1, 1, Direction.up, 0, True, "Name 1")
+        player2 = Player(2, 1, 0, Direction.up, 0, True, "Name 2")
+        player3 = Player(3, 0, 0, Direction.up, 0, True, "Name 3")
+        players = [player1, player2, player3]
+        cells = [[Cell([player3]), Cell([player2])], [Cell([]), Cell([player1])]]
+        game = Game(2, 2, cells, players, 2, True, datetime.now())
+
+        result = game.get_other_players(player2)
+
+        self.assertEqual([player1, player3], result)
+
+    def test_copying_a_game_should_return_same_game_but_different_identity(self):
+        player1 = Player(1, 1, 1, Direction.up, 0, True, "Name")
+        player2 = Player(2, 1, 0, Direction.up, 0, True, "Name2")
+        player3 = Player(3, 0, 0, Direction.up, 0, True, "Name3")
+        players = [player1, player2, player3]
+        cells = [[Cell([player3]), Cell([player2])], [Cell([]), Cell([player1])]]
+        game = Game(2, 2, cells, players, 2, True, datetime.now())
+
+        result = game.copy()
+
+        self.assertEqual(game, result)
+        self.assertNotEqual(id(game), id(result))
