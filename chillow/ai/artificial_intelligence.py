8686a67c093ebf1b5f81febb0714d11bfa629d03
Florian Trei <florian.trei@uni-oldenburg.de>
2020-10-31 14:19:15 +0100

    pickle nutzen

--- chillow/ai/artificial_intelligence.py
+++ chillow/ai/artificial_intelligence.py
@@ -1,4 +1,4 @@
-import copy
+import pickle
 from abc import ABCMeta, abstractmethod
 from typing import List
 
@@ -14,15 +14,16 @@ class ArtificialIntelligence(metaclass=ABCMeta):
     def __init__(self, player: Player):
         self.player = player
         self.turn_ctr = 0
+        self.max_speed = 10
 
     @abstractmethod
     def create_next_action(self, game: Game) -> Action:
         self.turn_ctr += 1
 
-    def find_surviving_actions(self, game_service: GameService) -> [Action]:
+    def find_surviving_actions(self, game_service: GameService) -> List[Action]:
         result: List[Action] = []
         for action in Action:  # select a surviving action
-            gs_copy = copy.deepcopy(game_service)
+            gs_copy = pickle.loads(pickle.dumps(game_service))
             try:
                 player = gs_copy.game.get_player_by_id(self.player.id)
                 if player.speed == self.max_speed and action == Action.speed_up:
