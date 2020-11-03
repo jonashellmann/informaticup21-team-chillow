6817bd273727bdb67ab2d2ee7e06baf1f9a2847d
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-03 13:03:15 +0100

    Implementiere KI als Kombination aus SearchTree und Pathfinding

---
+++ chillow/ai/pathfinding_search_tree_ai.py
@@ -0,0 +1,29 @@
+from copy import deepcopy
+
+from chillow.ai.pathfinding_ai import PathfindingAI
+from chillow.ai.search_tree_node import SearchTreeRoot
+from chillow.model.action import Action
+from chillow.model.game import Game
+from chillow.model.player import Player
+
+
+class PathfindingSearchTreeAI(PathfindingAI):
+
+    def __init__(self, player: Player, game: Game, max_speed, count_paths_to_check, depth: int):
+        super().__init__(player, game, max_speed, count_paths_to_check)
+        self.__depth = depth
+
+    def create_next_action(self, game: Game) -> Action:
+        super().create_next_action(game)
+        root = SearchTreeRoot(game.copy())
+        combinations = Action.get_combinations(len(game.get_other_players(self.player)))
+
+        surviving_actions = []
+
+        for action in Action.get_actions():
+            if root.calculate_action(self.player, combinations, self.__depth, self.turn_ctr, True, [action],
+                                     self.max_speed, True) is not None:
+                surviving_actions.append(action)
+
+        return self.find_action_by_best_path_connection(surviving_actions) if len(
+                surviving_actions) > 0 else Action.get_random_action()
