494e75e877dc27e707aab1b6a5b28f27ba9459b7
Florian Trei <florian.trei@uni-oldenburg.de>
2020-11-07 22:16:36 +0100

    Tests

--- tests/ai/test_pathfinding_search_tree_ai.py
+++ tests/ai/test_pathfinding_search_tree_ai.py
@@ -1,20 +1,45 @@
 import unittest
+from datetime import datetime
 
-import tests
-from chillow.ai.search_tree_pathfinding_ai import SearchTreePathfindingAI
+
 from chillow.model.action import Action
-from chillow.service.data_loader import JSONDataLoader
+from chillow.ai.pathfinding_search_tree_ai import PathfindingSearchTreeAI
+from chillow.model.cell import Cell
+from chillow.model.direction import Direction
+from chillow.model.game import Game
+from chillow.model.player import Player
 
 
 class PathfindingSearchTreeAITest(unittest.TestCase):
 
-    def setUp(self):
-        self.data_loader = JSONDataLoader()
+    def setUp(self) -> None:
+        player1 = Player(1, 0, 0, Direction.up, 1, True, "")
+        player2 = Player(2, 0, 1, Direction.down, 3, True, "")
+        players = [player1, player2]
+        cells = [[Cell([player1]), Cell(), Cell(), Cell()],
+                 [Cell([player2]), Cell(), Cell(), Cell()],
+                 [Cell(), Cell(), Cell(), Cell()],
+                 [Cell(), Cell(), Cell(), Cell()]]
 
-    def test_should_select_action_to_let_player_survive_next_two_rounds(self):
-        game = self.data_loader.load(tests.read_test_file("ai/game_4.json"))
-        sut = SearchTreePathfindingAI(game.you, 3, 100, 2)
+        game = Game(4, 4, cells, players, 2, True, datetime.now())
+        self.sut = PathfindingSearchTreeAI(player1, 2, 10, 2, 0.75)
 
-        result = sut.create_next_action(game)
+    def test_get_best_action_should_find_best_action(self):
+        action = self.sut.get_best_action([(Action.change_nothing, 10), (Action.speed_up, 8)], [Action.change_nothing])
 
-        self.assertEqual(Action.turn_left, result)
+        self.assertEqual(action, Action.change_nothing)
+
+    def test_get_best_action_shoud_find_best_action_when_first_best_action_ist_not_in_search_tree_list(self):
+        action = self.sut.get_best_action([(Action.change_nothing, 10), (Action.speed_up, 10)], [Action.speed_up])
+
+        self.assertEqual(action, Action.speed_up)
+
+    def test_get_best_action_shoud_find_best_action_with_tolerance(self):
+        action = self.sut.get_best_action([(Action.change_nothing, 10), (Action.speed_up, 8)], [Action.speed_up])
+
+        self.assertEqual(action, Action.speed_up)
+
+    def test_get_best_pathfinding_action_when_no_tolerance_matches(self):
+        action = self.sut.get_best_action([(Action.change_nothing, 10), (Action.speed_up, 8)], [Action.slow_down])
+
+        self.assertEqual(action, Action.change_nothing)
---
+++ tests/ai/test_search_tree_pathfinding_ai.py
@@ -0,0 +1,20 @@
+import unittest
+
+import tests
+from chillow.ai.search_tree_pathfinding_ai import SearchTreePathfindingAI
+from chillow.model.action import Action
+from chillow.service.data_loader import JSONDataLoader
+
+
+class SearchTreePathfindingAITest(unittest.TestCase):
+
+    def setUp(self):
+        self.data_loader = JSONDataLoader()
+
+    def test_should_select_action_to_let_player_survive_next_two_rounds(self):
+        game = self.data_loader.load(tests.read_test_file("ai/game_4.json"))
+        sut = SearchTreePathfindingAI(game.you, 3, 100, 2)
+
+        result = sut.create_next_action(game)
+
+        self.assertEqual(Action.turn_left, result)
