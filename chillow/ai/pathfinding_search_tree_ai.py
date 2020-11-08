e9f204773802fe0a653ccb2818411b40f779f345
Florian Trei <florian.trei@uni-oldenburg.de>
2020-11-08 14:26:43 +0100

    ungenutzten Import entfernen

--- chillow/ai/pathfinding_search_tree_ai.py
+++ chillow/ai/pathfinding_search_tree_ai.py
@@ -2,7 +2,6 @@ from typing import List, Tuple, Optional
 
 from chillow.ai.pathfinding_ai import PathfindingAI
 from chillow.ai.search_tree_ai import SearchTreeAI
-from chillow.ai.search_tree_node import SearchTreeRoot
 from chillow.model.action import Action
 from chillow.model.game import Game
 from chillow.model.player import Player
