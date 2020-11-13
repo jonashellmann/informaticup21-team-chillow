e90b83afbbcc10f201b0fa03bb9dab9b0d8c1888
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-13 11:09:00 +0100

    Pruefe Farben-Array schon im Konstruktor

--- chillow/view/view.py
+++ chillow/view/view.py
@@ -9,6 +9,8 @@ class View(metaclass=ABCMeta):
     def __init__(self, colors: List[Any]):
         self._interface_initialized = False
         self._player_colors = {0: (0, 0, 0)}
+
+        assert self.__colors is not None and len(self.__colors) > 0, "No colors available for interface"
         self.__colors = colors
 
     @abstractmethod
@@ -24,8 +26,6 @@ class View(metaclass=ABCMeta):
         raise NotImplementedError
 
     def _initialize_interface(self, game: Game):
-        assert self.__colors is not None and len(self.__colors) > 0, "No colors available for interface"
-
         self._interface_initialized = True
         for i in range(0, len(game.players)):
             self._player_colors[int(game.players[i].id)] = self.__colors[i % (len(self.__colors) - 1)]
