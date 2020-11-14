6017c885d0d7763bbc1564117f2e5a3fc98a1978
Florian Trei <flo.trei@googlemail.com>
2020-11-14 11:00:23 +0100

    Richtge Variable benutzen

--- chillow/view/view.py
+++ chillow/view/view.py
@@ -10,7 +10,7 @@ class View(metaclass=ABCMeta):
         self._interface_initialized = False
         self._player_colors = {0: (0, 0, 0)}
 
-        assert self.__colors is not None and len(self.__colors) > 0, "No colors available for interface"
+        assert colors is not None and len(colors) > 0, "No colors available for interface"
         self.__colors = colors
 
     @abstractmethod
