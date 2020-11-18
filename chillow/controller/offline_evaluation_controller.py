127537d022027cb975e173f6fed445c0c0282b55
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-18 14:01:12 +0100

    Erzeuge zufaelliges Spiel fuer Evaluation

--- chillow/controller/offline_evaluation_controller.py
+++ chillow/controller/offline_evaluation_controller.py
@@ -1,4 +1,12 @@
+from datetime import datetime, timedelta
+from random import randint
+
 from chillow.controller import OfflineController
+from chillow.model.cell import Cell
+from chillow.model.direction import Direction
+from chillow.model.game import Game
+from chillow.model.player import Player
+from chillow.service.ai import *
 from chillow.view.headless_view import HeadlessView
 
 
@@ -35,3 +43,34 @@ class OfflineEvaluationController(OfflineController):
         print(self.__participants)
         print("------- Winners --------")
         print(self.__results)
+
+    def _create_game(self) -> None:
+        height = randint(30, 70)
+        width = randint(30, 70)
+
+        player_count = randint(3, 6)
+        players = []
+        for i in range(player_count):
+            player = Player(i, randint(0, width - 1), randint(0, height - 1), Direction.get_random_direction(), 1, True,
+                            str(i))
+            players.append(player)
+
+        cells = [[Cell() for _ in range(width)] for _ in range(height)]
+        for player in players:
+            cells[player.y][player.x] = Cell([player])
+
+        self._game = Game(width, height, cells, players, 1, True, datetime.now() + timedelta(0, 180))
+
+        self._ais = []
+
+        self._ais.append(PathfindingAI(players[0], randint(1, 3), randint(1, 3) * 5 + 70))
+        self._ais.append(PathfindingSearchTreeAI(players[1], randint(1, 3), randint(1, 3) * 5 + 70, randint(2, 4), 0.75,
+                                                 randint(1, 3) * 10))
+        self._ais.append(SearchTreePathfindingAI(players[2], randint(1, 3), randint(1, 3) * 5 + 70, randint(2, 4),
+                                                 randint(1, 3) * 10))
+        if player_count > 3:
+            self._ais.append(SearchTreeAI(players[3], randint(1, 3), randint(2, 4), True, randint(1, 3) * 10))
+            if player_count > 4:
+                self._ais.append(NotKillingItselfAI(players[4], [AIOptions.max_distance], randint(1, 3), 0))
+                if player_count > 5:
+                    self._ais.append(RandomAI(players[5], randint(1, 3)))
