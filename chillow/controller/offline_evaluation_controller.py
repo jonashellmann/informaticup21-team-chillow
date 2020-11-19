12f3e3c2c38691426bccb7c739e692e370800f7f
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-19 09:27:12 +0100

    Speichere Evaluation in SQLite Datenbank

--- chillow/controller/offline_evaluation_controller.py
+++ chillow/controller/offline_evaluation_controller.py
@@ -1,5 +1,6 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from random import randint
+import sqlite3
 
 from chillow.controller import OfflineController
 from chillow.model.cell import Cell
@@ -14,43 +15,45 @@ class OfflineEvaluationController(OfflineController):
 
     def __init__(self, runs: int):
         super().__init__(HeadlessView())
-        self.__participants = {}
-        self.__results = {}
         self.__runs = runs
 
-    def play(self):
-        for i in range(self.__runs):
-            print("Round: " + str(i + 1))
+        self.__connection = sqlite3.connect("evaluation.db")
+        self.__cursor = self.__connection.cursor()
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS games (id INTEGER, width INTEGER, height INTEGER, date TEXT)")
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS participants (id INTEGER, game_id INTEGER, class TEXT,"
+                              "info TEXT)")
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS winners (id INTEGER, game_id INTEGER)")
 
+    def play(self):
+        max_game_id = self.__cursor.execute("SELECT MAX(id) FROM games").fetchone()[0]
+        if max_game_id is None:
+            max_game_id = 0
+
+        for i in range(self.__runs):
             super().play()
+
+            game_id = i + 1 + max_game_id
+            self.__cursor.execute("INSERT INTO games VALUES ({}, {}, {}, '{}')"
+                                  .format(game_id, self._game.width, self._game.height, datetime.now(timezone.utc)))
 
             winner_player = self._game.get_winner()
             for ai in self._ais:
+                ai_class = ai.__class__.__name__
                 ai_info = ai.get_information()
 
                 # Save how often an AI configuration participated in a game
-                if ai_info in self.__participants:
-                    self.__participants[ai_info] += 1
-                else:
-                    self.__participants[ai_info] = 1
+                self.__cursor.execute("INSERT INTO participants VALUES ({}, {}, '{}', '{}')"
+                                      .format(ai.player.id, game_id, ai_class, ai_info))
 
                 # Save how often an AI configuration won a game
                 if ai.player == winner_player:
-                    if ai_info in self.__results:
-                        self.__results[ai_info] += 1
-                    else:
-                        self.__results[ai_info] = 1
-
-        print("----- Participants -----")
-        print(self.__participants)
-        print("------- Winners --------")
-        print(self.__results)
+                    self.__cursor.execute("INSERT INTO winners VALUES ({}, {})".format(ai.player.id, game_id))
 
     def _create_game(self) -> None:
-        height = randint(30, 70)
-        width = randint(30, 70)
+        height = randint(15, 20)
+        width = randint(15, 20)
 
-        player_count = randint(3, 6)
+        player_count = randint(3, 4)
         players = []
         for i in range(player_count):
             player = Player(i, randint(0, width - 1), randint(0, height - 1), Direction.get_random_direction(), 1, True,
@@ -61,17 +64,15 @@ class OfflineEvaluationController(OfflineController):
         for player in players:
             cells[player.y][player.x] = Cell([player])
 
-        self._game = Game(width, height, cells, players, 1, True, datetime.now() + timedelta(0, 180))
+        self._game = Game(width, height, cells, players, 1, True, datetime.now() + timedelta(10, 15))
 
         self._ais = []
 
-        self._ais.append(PathfindingAI(players[0], randint(1, 3), randint(1, 3) * 5 + 70))
-        self._ais.append(PathfindingSearchTreeAI(players[1], randint(1, 3), randint(1, 3) * 5 + 70, randint(2, 4), 0.75,
-                                                 randint(1, 3) * 10))
-        self._ais.append(SearchTreePathfindingAI(players[2], randint(1, 3), randint(1, 3) * 5 + 70, randint(2, 4),
-                                                 randint(1, 3) * 10))
+        self._ais.append(PathfindingAI(players[0], 2, 75))
+        self._ais.append(NotKillingItselfAI(players[1], [AIOptions.max_distance], randint(1, 3), 0))
+        self._ais.append(NotKillingItselfAI(players[2], [AIOptions.max_distance], randint(1, 3), 0))
         if player_count > 3:
-            self._ais.append(SearchTreeAI(players[3], randint(1, 3), randint(2, 4), True, randint(1, 3) * 10))
+            self._ais.append(NotKillingItselfAI(players[3], [AIOptions.max_distance], randint(1, 3), 0))
             if player_count > 4:
                 self._ais.append(NotKillingItselfAI(players[4], [AIOptions.max_distance], randint(1, 3), 0))
                 if player_count > 5:
