d2e13955576e707dd2085ff429aee7868677d2b9
Florian Trei <flo.trei@googlemail.com>
2020-11-19 12:38:53 +0100

    Warnings entfernen

--- chillow/service/ai/artificial_intelligence.py
+++ chillow/service/ai/artificial_intelligence.py
@@ -1,12 +1,8 @@
-import pickle
 from abc import ABCMeta, abstractmethod
-from typing import List
 
-from chillow.exceptions import InvalidPlayerMoveException
 from chillow.model.action import Action
 from chillow.model.game import Game
 from chillow.model.player import Player
-from chillow.service.game_service import GameService
 
 
 class ArtificialIntelligence(metaclass=ABCMeta):
