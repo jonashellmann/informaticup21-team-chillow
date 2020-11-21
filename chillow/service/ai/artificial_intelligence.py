8183ef06781d181f1e9bb3cfc661b6fcda5d6a92
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-21 10:59:59 +0100

    Beschreibung der Art der Rueckgabe der AI-Berechnung als Kommentar in abstrakter Basisklasse

--- chillow/service/ai/artificial_intelligence.py
+++ chillow/service/ai/artificial_intelligence.py
@@ -15,6 +15,13 @@ class ArtificialIntelligence(metaclass=ABCMeta):
     def get_information(self) -> str:
         return "max_speed=" + str(self.max_speed)
 
+    # Important: To be able to share the result of this calculation across multiple processes, it is necessary
+    # that the result is stored as in integer in the "value"-variable of the "return_value"-Parameter.
+    # This means that no "return" statement is used.
+    # The integer value represents the index of the calculated action in the Action enumeration.
+    # For this transformation there are two methods:
+    # Action -> int: action.get_index()
+    # int -> Action: Action.get_by_index(index)
     @abstractmethod
     def create_next_action(self, game: Game, return_value: Value):
         raise NotImplementedError
