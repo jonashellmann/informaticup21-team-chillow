8a891832a93a2453cfcbd47ccb054f267964d6ca
Florian Trei <flo.trei@googlemail.com>
2020-11-25 18:56:26 +0100

    NotKillingItselfAi dokumentieren

--- chillow/service/ai/not_killing_itself_ai.py
+++ chillow/service/ai/not_killing_itself_ai.py
@@ -19,9 +19,21 @@ class AIOptions(Enum):
 
 
 class NotKillingItselfAI(ArtificialIntelligence):
+    """ Class that represents an AI that chooses actions that don't lose by letting it predict many moves into the
+        future up to a depth without considering the opponent's player actions. """
 
     def __init__(self, player: Player, options: List[AIOptions], max_speed: int, max_worse_distance: int,
                  depth: int):
+        """ Constructor that initializes the necessary attributes.
+
+        Args:
+            player: The player assigned to the AI.
+            options: List of possible options to change the behavior of the AI.
+            max_speed: The maximum speed the AI can reach.
+            max_worse_distance: A tolerance, whereby more than just the best action is calculated. Actions which are
+            worse, but within this tolerance, are also considered.
+            depth: Number of player actions that are looked into the future.
+        """
         super().__init__(player, max_speed)
         self.options = options
         self.max_worse_distance = max_worse_distance
@@ -52,6 +64,16 @@ class NotKillingItselfAI(ArtificialIntelligence):
 
     def calc_action_with_max_distance_to_visited_cells(self, game_service: GameService,
                                                        actions: List[Action]) -> List[Action]:
+        """ Calculates a list of actions that have the property to have as many free cells as possible in front of them
+            while running straight after the action has been executed.
+
+        Args:
+            game_service: GameService to simulate the actions.
+            actions: List of actions to be considered.
+
+        Returns:
+            List of best actions with the property having as many free cells as possible in front of the player.
+        """
         max_straight_distance = 0
         best_actions: Dict[Action, int] = {}
         for action in actions:
@@ -89,6 +111,16 @@ class NotKillingItselfAI(ArtificialIntelligence):
         return list(best_actions.keys())
 
     def find_surviving_actions(self, game_service: GameService, depth: int) -> List[Action]:
+        """ Finds a list of surviving actions with precalculating player moves into the future by simulating the actions
+            with the GameService.
+
+        Args:
+            game_service: GameService to simulate the actions.
+            depth: Number of moves to be precalculated into the future.
+
+        Returns:
+            List of surviving actions.
+        """
         result: List[Action] = []
         for action in Action:  # select a surviving action
             gs_copy = pickle.loads(pickle.dumps(game_service))
@@ -110,6 +142,15 @@ class NotKillingItselfAI(ArtificialIntelligence):
         return result
 
     def find_surviving_actions_with_best_depth(self, game_service: GameService) -> List[Action]:
+        """ Finds a list of surviving actions with precalculating player moves into the future by simulating the actions
+            with the GameService. Reduces the number of pre-calculated player moves until surviving actions are found.
+
+        Args:
+            game_service: GameService to simulate the actions.
+
+        Returns:
+            List of surviving actions.
+        """
         result: List[Action] = []
         for current_depth in reversed(range(1, self.depth + 1)):
             result = self.find_surviving_actions(game_service, current_depth)
