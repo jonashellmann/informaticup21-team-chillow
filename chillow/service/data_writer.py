30ea53adeb99944b78f90e677a2587da620e5f52
Jonas Hellmann <hellmann.jonas@web.de>
2020-11-25 17:25:16 +0100

    Dokumentiere DataWriter-Klasse

--- chillow/service/data_writer.py
+++ chillow/service/data_writer.py
@@ -5,13 +5,33 @@ from chillow.model.action import Action
 
 
 class DataWriter(metaclass=ABCMeta):
+    """Converts an object to a string."""
 
     @abstractmethod
     def write(self, action: Action) -> str:
+        """Converts an action to a string.
+
+        Args:
+            action: The action to be converted.
+
+        Returns:
+            The action as a string.
+        """
+
         raise NotImplementedError
 
 
 class JSONDataWriter(DataWriter):
+    """Converts an object to a JSON string."""
 
     def write(self, action: Action) -> str:
+        """Converts an action to a JSON string.
+
+        Args:
+            action: The action to be converted.
+
+        Returns:
+            The action as a JSON string.
+        """
+
         return json.dumps({"action": action.name})
