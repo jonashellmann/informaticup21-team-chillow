5cd624a383ec54c1e489ada025b7f4eaeb72e719
Florian Trei <flo.trei@googlemail.com>
2020-11-25 15:24:08 +0100

    Restliche Klasse testen

--- tests/view/test_console_view.py
+++ tests/view/test_console_view.py
@@ -1,7 +1,14 @@
+import io
 import unittest
+from datetime import datetime
 from unittest.mock import patch
 
+
 from chillow.model.action import Action
+from chillow.model.cell import Cell
+from chillow.model.direction import Direction
+from chillow.model.game import Game
+from chillow.model.player import Player
 from chillow.view.console_view import ConsoleView
 
 
@@ -43,3 +50,53 @@ class ConsoleViewTest(unittest.TestCase):
         sut = ConsoleView()
 
         self.assertTrue(sut.read_next_action(), Action.change_nothing)
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_correct_output_on_end(self,  mock_stdout):
+        sut = ConsoleView()
+        sut.end()
+        self.assertTrue("Game ended!" in str(mock_stdout.getvalue()))
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_correct_output_round(self,  mock_stdout):
+        player1 = Player(1, 0, 0, Direction.up, 1, True, "p1")
+        player2 = Player(2, 0, 1, Direction.down, 3, True, "")
+        players = [player1, player2]
+        cells = [[Cell([player1]), Cell(), Cell()],
+                 [Cell([player2]), Cell(), Cell()],
+                 [Cell(), Cell(), Cell()]]
+        game = Game(3, 3, cells, players, 2, True, datetime.now())
+        sut = ConsoleView()
+
+        sut.update(game)
+        self.assertTrue("Round :  0" in str(mock_stdout.getvalue()))
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_correct_output_winner(self, mock_stdout):
+        player1 = Player(1, 0, 0, Direction.up, 1, True, "p1")
+        player2 = Player(2, 0, 1, Direction.down, 3, False, "")
+        players = [player1, player2]
+        cells = [[Cell([player1]), Cell(), Cell()],
+                 [Cell([player2]), Cell(), Cell()],
+                 [Cell(), Cell(), Cell()]]
+        game = Game(3, 3, cells, players, 2, False, datetime.now())
+
+        sut = ConsoleView()
+        sut.update(game)
+
+        self.assertTrue("Winner: Player 1 (p1). Your player ID was 2" in str(mock_stdout.getvalue()))
+
+    @unittest.mock.patch('sys.stdout', new_callable=io.StringIO)
+    def test_correct_output_no_winner(self, mock_stdout):
+        player1 = Player(1, 0, 0, Direction.up, 1, False, "p1")
+        player2 = Player(2, 0, 1, Direction.down, 3, False, "")
+        players = [player1, player2]
+        cells = [[Cell([player1]), Cell(), Cell()],
+                 [Cell([player2]), Cell(), Cell()],
+                 [Cell(), Cell(), Cell()]]
+        game = Game(3, 3, cells, players, 2, False, datetime.now())
+
+        sut = ConsoleView()
+        sut.update(game)
+
+        self.assertTrue("No winner in game." in str(mock_stdout.getvalue()))
