05854d3cccac84394a3a4c7c957f820746cd62bf
Jonas Hellmann <hellmann.jonas@web.de>
2020-12-13 23:02:55 +0100

    SQL Listings angepasst

--- dokumentation/Dokumente/ai_evaluation_1.sql
+++ dokumentation/Dokumente/ai_evaluation_1.sql
@@ -1,46 +1,40 @@
 SELECT
-    class as Klasse,
-    Gewonnene_Spiele as "Gewonnene Spiele",
-    Gespielte_Spiele as "Gespielte Spiele",
-    ROUND((CAST(Gewonnene_Spiele as double) * 100 /
-           CAST(Gespielte_Spiele as double)), 2) as "Gewinnrate (%)",
-    Max_Ausfuerungszeit_in_Sek as "Max Zeit (Sek)",
-    Avg_Ausfuerungszeit_in_Sek as "Avg Zeit (Sek)",
+    class AS Klasse,
+    Gewonnene_Spiele AS "Gewonnene Spiele",
+    Gespielte_Spiele AS "Gespielte Spiele",
+    ROUND((CAST(Gewonnene_Spiele AS DOUBLE) * 100 /
+           CAST(Gespielte_Spiele AS DOUBLE)), 2) AS "Gewinnrate (%)",
+    Max_Ausfuerungszeit_in_Sek AS "Max Zeit (Sek)",
+    Avg_Ausfuerungszeit_in_Sek AS "Avg Zeit (Sek)",
     Avg_Ausfuerungszeit_in_Sek_ohne_deadline "Avg Zeit oD (Sek)",
-    ROUND((CAST(Anzahl_Deadline_nicht_ueberschritten as double) * 100 /
-           CAST(Anzahl_Executions as double)), 2) as "Deadline eingehalten (%)"
+    ROUND((CAST(Anzahl_Deadline_nicht_ueberschritten AS DOUBLE) * 100 /
+           CAST(Anzahl_Executions AS DOUBLE)), 2) AS "Deadline eingehalten (%)"
 FROM (
     SELECT
         class,
-        (SELECT COUNT(winner_id)
-            FROM games g
+        (SELECT COUNT(winner_id) FROM games g
             JOIN players ssp ON ssp.id = g.winner_id
-            WHERE ssp.class = p.class) as Gewonnene_Spiele,
-        (SELECT COUNT(class)
-            FROM participants pp
+            WHERE ssp.class = p.class) AS Gewonnene_Spiele,
+        (SELECT COUNT(class) FROM participants pp
             JOIN players ssp ON ssp.id = pp.player_id
-            WHERE ssp.class = p.class) as Gespielte_Spiele,
-        (SELECT ROUND(MAX(execution), 4)
-            FROM execution_times e
+            WHERE ssp.class = p.class) AS Gespielte_Spiele,
+        (SELECT ROUND(MAX(execution), 4) FROM execution_times e
             JOIN players ssp ON ssp.id = e.player_id
-            WHERE ssp.class = p.class) as Max_Ausfuerungszeit_in_Sek,
-        (SELECT ROUND(AVG(execution), 4)
-            FROM execution_times e
+            WHERE ssp.class = p.class) AS Max_Ausfuerungszeit_in_Sek,
+        (SELECT ROUND(AVG(execution), 4) FROM execution_times e
             JOIN players ssp ON ssp.id = e.player_id
             WHERE ssp.class = p.class) as Avg_Ausfuerungszeit_in_Sek,
-        (SELECT ROUND(AVG(execution), 4)
-            FROM execution_times e
+        (SELECT ROUND(AVG(execution), 4) FROM execution_times e
             JOIN players ssp ON ssp.id = e.player_id
-            WHERE ssp.class = p.class AND execution < 60) as Avg_Ausfuerungszeit_in_Sek_ohne_deadline,
-        (SELECT COUNT(class)
-            FROM execution_times e
+            WHERE ssp.class = p.class AND execution < 60) AS Avg_Ausfuerungszeit_in_Sek_ohne_deadline,
+        (SELECT COUNT(class) FROM execution_times e
             JOIN players ssp ON ssp.id = e.player_id
-            WHERE ssp.class = p.class) as Anzahl_Executions,
-        (SELECT COUNT(class)
-            FROM execution_times e JOIN players ssp ON ssp.id = e.player_id
+            WHERE ssp.class = p.class) AS Anzahl_Executions,
+        (SELECT COUNT(class) FROM execution_times e
+            JOIN players ssp ON ssp.id = e.player_id
             WHERE ssp.class = p.class
-            AND execution < 60) as Anzahl_Deadline_nicht_ueberschritten
+            AND execution < 60) AS Anzahl_Deadline_nicht_ueberschritten
     FROM players p
-    Group BY p.class
-) as result
-ORDER BY "Gewinnrate in %" DESC
+    GROUP BY p.class
+) AS result
+ORDER BY "Gewinnrate (%)" DESC
